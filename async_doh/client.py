c663b8a2676c58ad839e1e4a73f22cba9a5391a5
Gerald <gera2ld@live.com>
2020-08-12 14:13:19 +0800

    feat: add support for POST requests

--- async_doh/client.py
+++ async_doh/client.py
@@ -20,33 +20,53 @@ class DoHClient:
             self._session_task = None
             await session.__aexit__(None, None, None)
 
+    async def request_message(self, url, req, method='GET'):
+        headers = {
+            'accept': 'application/dns-message',
+            'content-type': 'application/dns-message',
+        }
+        message = req.pack()
+        if method == 'GET':
+            dns = base64.urlsafe_b64encode(message).decode().rstrip('=')
+            params = { 'dns': dns }
+            data = None
+        else:
+            assert method == 'POST', f'Unsupported method: {method}'
+            params = None
+            data = message
+        async with self.session.request(method, url, params=params, data=data, headers=headers) as resp:
+            assert 200 <= resp.status < 300, f'Request error: {resp.status}'
+            data = await resp.read()
+            result = DNSMessage.parse(data)
+            return result
+
     async def query(self, url, name, qtype=types.A, method='GET'):
         req = DNSMessage(qr=REQUEST)
         if isinstance(qtype, str):
             qtype = types.get_code(qtype)
         req.qd = [Record(REQUEST, name, qtype)]
-        dns = base64.urlsafe_b64encode(req.pack()).decode().rstrip('=')
-        async with self.session.get(url,
-                params={ 'dns': dns },
-                headers={ 'accept': 'application/dns-message' }) as resp:
-            data = await resp.read()
-            result = DNSMessage.parse(data)
-            return result
+        return await self.request_message(url, req, method)
 
-    async def query_json(self, url, name, qtype=types.A, method='GET'):
+    async def query_json(self, url, name, qtype=types.A):
+        '''Query via JSON APIs for DNS over HTTPS.
+
+        All API calls are HTTP GET requests.
+        Reference: https://developers.google.com/speed/public-dns/docs/doh/json
+        '''
         if isinstance(qtype, int):
             qtype = types.get_name(qtype)
         async with self.session.get(url,
                 params={ 'name': name, 'qtype': qtype },
                 headers={ 'accept': 'application/dns-json' }) as resp:
+            assert 200 <= resp.status < 300, f'Request error: {resp.status}'
             data = await resp.json(content_type=None)
             return data
 
 if __name__ == '__main__':
     async def main():
         async with DoHClient() as client:
-            result = await client.query('https://doh.pub/dns-query', 'www.google.com', 'A')
+            result = await client.query('http://localhost:8080/dns-query', 'www.google.com', 'A')
             print('query:', result)
-            result = await client.query_json('https://doh.pub/dns-query', 'www.google.com', 'A')
+            result = await client.query_json('http://localhost:8080/dns-query', 'www.google.com', 'A')
             print('query_json:', result)
     asyncio.run(main())
--- async_doh/server.py
+++ async_doh/server.py
@@ -1,4 +1,5 @@
 import base64
+import logging
 from aiohttp import web
 from async_dns import types, DNSMessage, get_nameservers
 from async_dns.resolver import ProxyResolver
@@ -10,46 +11,68 @@ def set_resolver(_resolver):
     global resolver
     resolver = _resolver
 
+async def handle_json_api(request):
+    '''JSON API for DNS over HTTPS
+
+    Reference: https://developers.google.com/speed/public-dns/docs/doh/json
+    '''
+    assert request.method == 'GET', web.HTTPMethodNotAllowed
+    accept = request.headers.get('accept')
+    assert accept == 'application/dns-json', web.HTTPBadRequest
+    name = request.query.get('name')
+    type = request.query.get('type', 'A')
+    cd = request.query.get('cd') in ('1', 'true')
+    ct = request.query.get('ct')
+    qtype = types.get_code(type)
+    assert qtype is not None, web.HTTPBadRequest
+    result = await resolver.query(name, qtype)
+    assert result is not None, web.HTTPInternalServerError
+    data = {
+        'Status': result.r,
+        'TC': result.tc,
+        'RD': result.rd,
+        'RA': result.ra,
+        'AD': False,
+        'CD': cd,
+        'Question': [
+            { 'name': name, 'type': qtype },
+        ],
+        'Answer': [
+            { 'name': item.name, 'type': item.qtype, 'TTL': item.ttl, 'data': item.data }
+            for item in result.an
+        ],
+    }
+    logging.info('[JSON][%s] %s %s %d', request.method, name, types.get_name(qtype), result.r)
+    return web.json_response(data, content_type=accept)
+
+async def handle_message_api(request):
+    accept = request.headers.get('accept')
+    assert accept == 'application/dns-message', web.HTTPBadRequest
+    if request.method == 'GET':
+        dns = request.query.get('dns')
+        dns += '=' * ((4 - len(dns) % 4) % 4)
+        dns = base64.urlsafe_b64decode(dns)
+    else:
+        assert request.method == 'POST', web.HTTPMethodNotAllowed
+        assert request.headers.get('content-type') == 'application/dns-message', web.HTTPBadRequest
+        dns = await request.read()
+    msg = DNSMessage.parse(dns)
+    for question in msg.qd:
+        result = await resolver.query(question.name, question.qtype)
+        result.qid = msg.qid
+        data = result.pack()
+        logging.info('[MSG][%s] %s %s %d', request.method, question.name, types.get_name(question.qtype), result.r)
+        break   # only one question is supported
+    return web.Response(body=data)
+
 @routes.get('/dns-query')
+@routes.post('/dns-query')
 async def doh(request):
     accept = request.headers.get('accept')
     if accept == 'application/dns-json':
-        # Ref: https://developers.google.com/speed/public-dns/docs/doh/json
-        name = request.query.get('name')
-        type = request.query.get('type', 'A')
-        cd = request.query.get('cd') in ('1', 'true')
-        ct = request.query.get('ct')
-        qtype = types.get_code(type)
-        assert qtype is not None, web.HTTPBadRequest
-        result = await resolver.query(name, qtype)
-        assert result is not None, web.HTTPInternalServerError
-        data = {
-            'Status': result.r,
-            'TC': result.tc,
-            'RD': result.rd,
-            'RA': result.ra,
-            'AD': False,
-            'CD': cd,
-            'Question': [
-                { 'name': name, 'type': qtype },
-            ],
-            'Answer': [
-                { 'name': item.name, 'type': item.qtype, 'TTL': item.ttl, 'data': item.data }
-                for item in result.an
-            ],
-        }
-        return web.json_response(data, content_type=accept)
+        return await handle_json_api(request)
     elif accept == 'application/dns-message':
-        dns = request.query.get('dns')
-        dns = base64.urlsafe_b64decode(dns)
-        msg = DNSMessage.parse(dns)
-        for question in msg.qd:
-            result, _cache = await resolver.query_with_cache(question.name, question.qtype)
-            assert result is not None, web.HTTPInternalServerError
-            result.qid = msg.qid
-            data = result.pack()
-            break   # only one question is supported
-        return web.Response(body=data)
+        return await handle_message_api(request)
     else:
         raise web.HTTPBadRequest
 
@@ -57,4 +80,5 @@ application = web.Application()
 application.add_routes(routes)
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
     web.run_app(application)
