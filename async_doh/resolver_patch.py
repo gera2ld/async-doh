afe1bf5435367700331a46c13ec71ba2bbf832e7
Gerald <gera2ld@live.com>
2020-08-13 22:39:59 +0800

    fix: use POST method by default

--- async_doh/resolver_patch.py
+++ async_doh/resolver_patch.py
@@ -3,13 +3,13 @@ from functools import partial
 from async_dns.resolver import Query
 from .client import DoHClient
 
-async def request_https(client, req, addr, timeout=3.0):
-    result = await client.request_message(str(addr), req)
+async def request_https(client, req, addr, timeout=3.0, method='POST'):
+    result = await client.request_message(str(addr), req, method)
     return result
 
-async def patch():
+async def patch(method='POST'):
     client = await DoHClient().__aenter__()
-    Query.protocols['https'] = partial(request_https, client)
+    Query.protocols['https'] = partial(request_https, client, method=method)
     async def revoke():
         Query.protocols.pop('https', None)
         await client.__aexit__(None, None, None)
